15e09bbd0f4b6c17e9adfb59ba37738335d595f0
deksaa <erikdestan@gmail.com>
2016-04-24 22:50:47 +0300

    Article data table
    Create Article data table and
    save process.

---
+++ src/main/java/com/blog/river/article/ArticleService.java
@@ -0,0 +1,11 @@
+package com.blog.river.article;
+
+import java.util.List;
+
+import com.blog.river.admin.Admin;
+
+public interface ArticleService {
+	public Article findByTitle(String title);
+	public Article save(Article article);
+	public List<Article> findAll();
+}
